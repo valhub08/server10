c70daf09df80f5c51a8578f54cc4a771d28da90d
valhub08 <jhstl1203@gmail.com>
2024-10-08 20:34:20 +0900

    first

---
+++ main/java/com/example/server10/Server10Application.java
@@ -0,0 +1,13 @@
+package com.example.server10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server10Application.class, args);
+    }
+
+}
