c83d1cbcecf342c02d5090f2c12ceef1652eac65
valhub08 <jhstl1203@gmail.com>
2024-10-10 20:18:31 +0900

    Build:RefreshTokenRepository (RefreshToken entity CRUD)

---
+++ main/java/com/example/server10/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package com.example.server10.repository;
+
+import com.example.server10.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByKey(String key);
+}
+
+
+
