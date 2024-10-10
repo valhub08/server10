f63139f06a64eb0e4a47c7cadbfabe754bde917f
valhub08 <jhstl1203@gmail.com>
2024-10-10 14:49:01 +0900

    SecretKey build

---
+++ main/java/com/example/server10/util/SecretKeyGenerator.java
@@ -0,0 +1,15 @@
+package com.example.server10.util;
+
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class SecretKeyGenerator {
+    public static void main(String[] args) {
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        String secret = Encoders.BASE64.encode(key.getEncoded());
+        System.out.println("Generated JWT Secret Key:");
+        System.out.println(secret);
+    }
+}
