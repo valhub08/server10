1b8b8a9d6714815b5da1cb4dcdd88d645ff613ae
valhub08 <jhstl1203@gmail.com>
2024-10-11 19:49:32 +0900

    Build:AuthService (authentication service)

---
+++ main/java/com/example/server10/service/AuthService.java
@@ -0,0 +1,107 @@
+package com.example.server10.service;
+
+import com.example.server10.dto.LoginDto;
+import com.example.server10.dto.MemberRequestDto;
+import com.example.server10.dto.MemberResponseDto;
+import com.example.server10.dto.TokenDto;
+import com.example.server10.dto.TokenRequestDto;
+import com.example.server10.entity.Member;
+import com.example.server10.entity.RefreshToken;
+import com.example.server10.jwt.TokenProvider;
+import com.example.server10.repository.MemberRepository;
+import com.example.server10.repository.RefreshTokenRepository;
+import com.example.server10.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    /**
+     * 회원가입 서비스
+     *
+     * @param memberRequestDto 회원가입 요청 DTO
+     * @return 회원가입 응답 DTO
+     */
+    @Transactional
+    public MemberResponseDto signup(MemberRequestDto memberRequestDto) {
+        if (memberRepository.existsByEmail(memberRequestDto.getEmail())) {
+            throw new CustomException("이미 가입되어 있는 유저입니다.");
+        }
+
+        Member member = memberRequestDto.toMember(passwordEncoder);
+        return MemberResponseDto.of(memberRepository.save(member));
+    }
+
+    /**
+     * 로그인 서비스
+     *
+     * @param loginDto 로그인 요청 DTO
+     * @return 토큰 응답 DTO
+     */
+    @Transactional
+    public TokenDto login(LoginDto loginDto) {
+        // 1. Login ID/PW 를 기반으로 AuthenticationToken 생성
+        UsernamePasswordAuthenticationToken authenticationToken = loginDto.toAuthentication();
+
+        // 2. 실제로 검증 (사용자 비밀번호 체크) 이 이루어지는 부분
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 3. 인증 정보를 기반으로 JWT 토큰 생성
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+
+        // 4. RefreshToken 저장
+        RefreshToken refreshToken = RefreshToken.builder().key(authentication.getName()).value(tokenDto.getRefreshToken()).build();
+
+        refreshTokenRepository.save(refreshToken);
+
+        // 5. 토큰 발급
+        return tokenDto;
+    }
+
+    /**
+     * 토큰 재발급 서비스
+     *
+     * @param tokenRequestDto 토큰 재발급 요청 DTO
+     * @return 토큰 응답 DTO
+     */
+    @Transactional
+    public TokenDto reissue(TokenRequestDto tokenRequestDto) {
+        // 1. Refresh Token 검증
+        if (!tokenProvider.validateToken(tokenRequestDto.getRefreshToken())) {
+            throw new CustomException("Refresh Token이 유효하지 않습니다.");
+        }
+
+        // 2. Access Token에서 Member ID 가져오기
+        Authentication authentication = tokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
+
+        // 3. 저장소에서 Member ID를 기반으로 Refresh Token 값 가져옴
+        RefreshToken refreshToken = refreshTokenRepository.findByKey(authentication.getName()).orElseThrow(() -> new CustomException("로그아웃 된 사용자입니다."));
+
+        // 4. Refresh Token 일치하는지 검사
+        if (!refreshToken.getValue().equals(tokenRequestDto.getRefreshToken())) {
+            throw new CustomException("토큰의 유저 정보가 일치하지 않습니다.");
+        }
+
+        // 5. 새로운 토큰 생성
+        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+
+        // 6. 저장소 정보 업데이트
+        refreshToken.updateValue(tokenDto.getRefreshToken());
+        refreshTokenRepository.save(refreshToken);
+
+        // 토큰 발급
+        return tokenDto;
+    }
+}
