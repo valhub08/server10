9a28063c67cacf37d6cf0987fa3282acfd28e4ea
valhub08 <jhstl1203@gmail.com>
2024-10-16 21:49:52 +0900

    Fix: AuthSignupController.java.java(add swagger setting)

--- main/java/com/example/server10/controller/AuthSignupController.java
+++ main/java/com/example/server10/controller/AuthSignupController.java
@@ -3,6 +3,11 @@ package com.example.server10.controller;
 import com.example.server10.dto.MemberRequestDto;
 import com.example.server10.dto.MemberResponseDto;
 import com.example.server10.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -17,6 +22,12 @@ import jakarta.validation.Valid;
 public class AuthSignupController {
     private final AuthService authService;
 
+    @Operation(summary = "회원가입 요청", description = "사용자 회원가입 처리")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "회원가입 성공",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = MemberResponseDto.class))}),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청")
+    })
     /**
      * 회원가입 엔드포인트
      *
@@ -28,7 +39,5 @@ public class AuthSignupController {
         return ResponseEntity.ok(authService.signup(memberRequestDto));
     }
 }
-
-
 
 
