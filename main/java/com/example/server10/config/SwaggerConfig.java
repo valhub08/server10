bf9834eb5d3e58e028c8f962fec04ee99474eb94
valhub08 <jhstl1203@gmail.com>
2024-10-16 21:57:12 +0900

    build: SwaggerConfig.java(SwaggerConfig build)

---
+++ main/java/com/example/server10/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.example.server10.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "AirDns App",
+                description = "AirDns App Api 명세서",
+                version = "v1"),
+        security = @SecurityRequirement(name = "Bearer Authentication")  // API에 JWT 인증이 필요함을 명시
+)
+@io.swagger.v3.oas.annotations.security.SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "Bearer"
+)
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components());
+    }
+
+}
