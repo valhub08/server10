2c8b516a2007c0eef07985f0ef6e7a35926d9730
valhub08 <jhstl1203@gmail.com>
2024-10-22 23:23:55 +0900

    update: SwaggerConfig.java(update class)

--- main/java/com/example/server10/config/SwaggerConfig.java
+++ main/java/com/example/server10/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(
         info = @Info(title = "AirDns App",
                 description = "AirDns App Api 명세서",
-                version = "version1"),
+                version = "version 1"),
         security = @SecurityRequirement(name = "Bearer Authentication")  // API에 JWT 인증이 필요함을 명시
 )
 @io.swagger.v3.oas.annotations.security.SecurityScheme(
